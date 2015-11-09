7fffb44f2f3d72ee013477fd4a9632114dc15a14
frances harlow <frances.harlow@gmail.com>
2015-11-08 19:21:06 -0500

    identifies matching pairs during a turn

--- main.js
+++ main.js
@@ -1,80 +1,106 @@
 console.log("main is linked");
 
+// Pseudocoding
+// add eventListeners to the individual boxes aka 'cards' so that, when clicked,
+// they 'flip over' to show an img
+// each turn consists of 2 clicks, then if they are not a match, both cards flip back over and the images are no longer visible
+// when the imgs are a match, they remain visible
+// the game ends when all the pairs have been identified
+
 var resetButton = document.querySelector('#reset');
 var allCards = document.querySelectorAll('.card');
 
 var player = {
+  firstClickImg: '',
+  secondClickImg: '',
   firstClick: '',
   secondClick: '',
   reset: function(){
     this.firstClick = undefined;
     this.secondClick = undefined;
+    this.firstClickImg = undefined;
+    this.secondClickImg = undefined;
   }
 };
 
 var counter = 0;
 
-var Card = function Card (imgSrc, element) {
-  this.imgSrc = imgSrc;
-  this.element = element;
-}
+// var Card = function Card (imgSrc, element) {
+//   this.imgSrc = imgSrc;
+//   this.element = element;
+// };
 
 var cardDeck;
-var newCard;
 var newCardArray = [];
 
-var beyonces = ['img/beyonce_1.gif','img/beyonce_2.gif','img/beyonce_3.gif','img/beyonce_4.gif','img/beyonce_5.gif','img/beyonce_6.gif']
-
-//add eventListeners to the individual boxes aka 'cards' so that, when clicked,
-// they 'flip over' to show an img
-
-//each 'turn' consists of 2 clicks, then both cards flip back over and the images are no longer visible
-
-//when the imgs are the same, the eventListener is removed so they stay visible
-
-//the game ends when all the pairs have been identified
+var beyonces = ['img/beyonce_1.gif','img/beyonce_2.gif','img/beyonce_3.gif','img/beyonce_4.gif','img/beyonce_5.gif','img/beyonce_6.gif'];
 
 var setUpBoard = function(){
   // take a random sample of the imgs that is equal to half the total number of cards
   var randomBeyonceSample = _.sample(beyonces,(allCards.length / 2));
   // create new array in which each item in the random sample appears exactly twice
-  var cardDeck = _.flatten([randomBeyonceSample,randomBeyonceSample]);
+  cardDeck = _.flatten([randomBeyonceSample,randomBeyonceSample]);
   //reshuffle the array
   _.shuffle(cardDeck);
-  // iterate through the array, creating a new img for each item, give each img a corresponding src, add to it the class of hidden
+  // iterate through the array, creating a new img for each item, giving each img a corresponding src, adding to it the class of hidden
   for (var i = 0; i < cardDeck.length; i ++){
-    newCard = document.createElement('img');
+    var newCard = document.createElement('img');
     newCard.src = cardDeck[i];
     newCard.classList.add('hidden');
     // push the newly formed card to another array so it can be appended to each item in allCards
-    // append each card, with its img, src, and class of hidden, to an existing card div 
     newCardArray.push(newCard);
+    // append each card, with its img, src, and class of hidden, to an existing card div 
     allCards[i].appendChild(newCardArray[i]);
+    // add an event listener to each card so that, when clicked, it removes the class of hidden
     allCards[i].addEventListener('click',function () {
-      event.target.children[0].classList.remove('hidden');
+      cardDeck = event.target.children[0]
+      cardDeck.classList.remove('hidden');
+      // upon, add the img src to the player object key called firstClick
       if (counter === 0) {
-        player.firstClick = event.target.children[0].src;
+        player.firstClickImg = cardDeck.src;
+        player.firstClick = cardDeck;
         counter++;
         console.log(counter);
         console.log(player);
+      // add the second click's img src to the player object key called secondClick
       } else if (counter === 1) {
-        player.secondClick = event.target.children[0].src;
-        console.log(counter);
-        console.log(player);
-      };
-      // if (player.firstClick !== player.secondClick) {
-      //   window.setTimeout(event.target.children[0].classList.add('hidden'),1000);
-      // } 
+        player.secondClickImg = cardDeck.src;
+        player.secondClick = cardDeck;
+        counter++;
+        // console.log(counter);
+        // console.log(player.firstClick);
+        // console.log(player.secondClick);
+          if (counter === 2 && player.firstClickImg !== player.secondClickImg) {
+          console.log(counter);
+          console.log(player.firstClick);
+          console.log(player.secondClick);
+          window.setTimeout(player.firstClick.classList.add('hidden'),2000);
+          window.setTimeout(player.secondClick.classList.add('hidden'),2000);
+        };
+      };  
     });
-  }
+  };
 };
 
 setUpBoard();
 
-var takeTurn = function(){
+// var takeTurn = function(event){
+//   if (counter === 0) {
+//     player.firstClick = cardDeck.src;
+//     counter++;
+//     console.log(counter);
+//     console.log(player);
+//   } else if (counter === 1) {
+//     player.secondClick = cardDeck.src;
+//     counter ++;
+//     console.log(counter);
+//     console.log(player);
+//   } else if (counter === 2 && player.firstClick !== player.secondClick) {
+//     window.setTimeout(cardDeck.classList.add('hidden'),1000);
+//   }
+// };
 
-}
-
+// takeTurn();
 
 // create a new array 'cards' containing card objects
 // var Card = function(imageSrc){
